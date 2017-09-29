a19177d0d7d700eaa075eccb9c829ef9c800daab
Antonio G. Bolaño <angonzalez@odec.es>
2017-09-29 13:14:47 +0200

    Navegando al detalle de un post

--- src/app/post-preview/post-preview.component.ts
+++ src/app/post-preview/post-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Post } from '../post';
 
@@ -30,6 +30,18 @@ export class PostPreviewComponent {
   | dicho clic se realiza en el template de este componente, necesitas,      |
   | además, un manejador para el mismo.                                      |
   |=========================================================================*/
+
+  // Este emisor de eventos permite notofocar cuando
+  // se ha hecho click sobre un post. Contiene la información
+  // del post que ha sido clickado.
+  @Output() postClickado = new EventEmitter<Post>();
+
+  // Este método s eejcuta cuando se realiza el click sobre un post en la plantilla
+  // y se encarga de emitir el evento de notificación.
+  notificarPostClickado(post: Post): void {
+    this.postClickado.emit(post);
+
+  }
 
   plainTextToHtml(text: string): string {
     return text ? `<p>${text.replace(/\n/gi, '</p><p>')}</p>` : '';
--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { Post } from '../post';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-posts-list',
@@ -8,6 +9,10 @@ import { Post } from '../post';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostsListComponent {
+
+  // inyectamos el Router como dependencia para
+  // poder navegar a otras páginas
+  constructor(private _router: Router) { }
 
   @Input() posts: Post[];
 
@@ -30,5 +35,11 @@ export class PostsListComponent {
   | app. La ruta a navegar es '/posts', pasando como parámetro el            |
   | identificador del post.                                                  |
   |=========================================================================*/
-
+  mostrarPost(post: Post): void {
+    // navigate permite nevegar a una ruta definida
+    // como la ruta está parametrizada, en el vector
+    // con los parámetros incluimos primero la ruta
+    // y luego, como parámetro de la ruta, el ID del post
+    this._router.navigate(['posts', post.id]);
+  }
 }
