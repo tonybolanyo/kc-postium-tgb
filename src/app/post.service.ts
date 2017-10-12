c9fa6f3fe64e47fa7c863acca92a9bde096cb377
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-10-12 23:11:34 +0200

    Agregado filtro de fecha futura en la llamada del servicio de posts y ordenados descendentemente por fecha de publicación (el mñas moderno primero)

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -32,7 +32,13 @@ export class PostService {
     | Una pista más, por si acaso: HttpParams.                                 |
     |=========================================================================*/
 
-    return this._http.get<Post[]>(`${environment.backendUri}/posts`);
+    const options = {
+      params: new HttpParams()
+        .set('_sort', 'publicationDate')
+        .set('_order', 'desc')
+        .set('publicationDate_lte', Date.now().toString())
+    };
+    return this._http.get<Post[]>(`${environment.backendUri}/posts`, options);
   }
 
   getUserPosts(id: number): Observable<Post[]> {
